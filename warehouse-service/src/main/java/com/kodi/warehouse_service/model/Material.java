7754c9071c594bed5c9b1e3fdf6fde79fe7deba9
dek <yundenis1982@gmail.com>
2024-11-11 01:02:09 +0900

    Initial commit

---
+++ warehouse-service/src/main/java/com/kodi/warehouse_service/model/Material.java
@@ -0,0 +1,12 @@
+package com.kodi.warehouse_service.model;
+
+import java.time.LocalDate;
+
+public class Material {
+	
+	private int codeitem;
+	private String name;
+	private LocalDate receivingday;
+	private LocalDate storageday;
+	private Typematerial type;
+}
---
+++ warehouse-service/src/main/java/com/kodi/warehouse_service/model/Typematerial.java
@@ -0,0 +1,5 @@
+package com.kodi.warehouse_service.model;
+
+public enum Typematerial {
+	PLATE, CONTAINER, INBOX, OUTBOX, FOREBEARS, GATOBASHI, LEAD, SINGLEBOX;
+}
